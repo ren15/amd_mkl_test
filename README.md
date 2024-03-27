25d130d5cb1b13f838259005cc183f4ff7f9350a
Ren Wang <iamwrm@gmail.com>
2024-03-27 13:32:02 +0800

    add tes

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 Test if ld_preload will boost numpy performance on AMD, i.e. github action runner
 
+https://danieldk.eu/Posts/2020-08-31-MKL-Zen.html
---
+++ test.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -ueo pipefail
+
+
+# check if lscpu outputs AMD
+
+cpu_vendor=$(lscpu | grep "Vendor ID:" | awk '{print $3}')
+
+# Check if the CPU vendor is AMD
+if [ "$cpu_vendor" == "AuthenticAMD" ]; then
+    echo "CPU vendor is AMD"
+else
+    exit 1
+fi
+
+
+cat <<EOF > fakeintel.c
+int mkl_serv_intel_cpu_true() {
+  return 1;
+}
+
+EOF
+
+gcc -shared -fPIC -o libfakeintel.so fakeintel.c
+
+conda install -c defaults --override-channels numpy
+
+echo "No LD_PRELOAD"
+python mkl_amd_hack.py
+
+echo "Has LD_PRELOAD"
+LD_PRELOAD=/path/to/libfakeintel.so python mkl_amd_hack.py
